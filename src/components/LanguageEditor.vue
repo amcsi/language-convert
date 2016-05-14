13b80a21af467fd13aa58b919596f016f9a604c5
Attila Szeremi <attila@szeremi.org>
2016-05-14 15:40:44 +0100

    Install bootstrap and set up basic view

---
+++ src/components/LanguageEditor.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>this is template body</div>
+
+    <textarea cols="20" rows="20"></textarea>
+</template>
+<script>
+  /** @class LanguageEditor */
+  const LanguageEditor = {
+    data () {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {}
+  }
+  export default LanguageEditor
+</script>
+<style scoped>
+  textarea {
+    width: 100%;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.css'
+
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
